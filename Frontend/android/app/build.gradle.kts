90c050e08f618016fa456a58aff85438e965517d
NiezTsuki <danieljofremolina@gmail.com>
2025-10-18 21:23:47 -0300

    arreglo apk

--- Frontend/android/app/build.gradle.kts
+++ Frontend/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.senior_locator_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.senior_locator_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,13 +29,17 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation("org.slf4j:slf4j-simple:1.7.32")
 }
---
+++ Frontend/android/app/proguard-rules.pro
@@ -0,0 +1,3 @@
+# Reglas para SLF4J (usado por Pusher)
+-keep class org.slf4j.** { *; }
+-keep class org.slf4j.impl.StaticLoggerBinder { *; }
